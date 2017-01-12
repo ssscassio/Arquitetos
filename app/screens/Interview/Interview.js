f0ba6f90f48623a85f22764fa73f255a62bb436c
Cassio Santos <sss.cassio@gmail.com>
2017-01-12 19:48:17 -0300

    Adicionado Json Inline do questionario

--- app/screens/Interview/Interview.js
+++ app/screens/Interview/Interview.js
@@ -35,49 +35,211 @@ export default class InterviewScreen extends Component {
             result: []
         }
 
-        questions = [{
-                id: 1,
-                person: {
-                    name: "Bruno Capanema",
-                    img: "https://www.investeducar.com.br/wp-content/uploads/2013/07/curso-de-departamento-pessoal-rotinas-diarias-300x300.jpg"
-                },
-                phrases: [
-                    "Olá, Bruno aqui, blz? Quero saber uma coisa:",
-                    "Como você calcula o valor do seu projeto ou serviço",
-                ],
-                answers: [
-                    {id: 1,
-                    text: "Olá, Bruno aqui, blz? Quero saber uma coisa:Olá, Bruno aqui, blz? Quero saber uma coisa:Olá, Bruno aqui, blz? Quero saber uma coisa:Olá, Bruno aqui, blz? Quero saber uma coisa:Olá, Bruno aqui, blz? Quero saber uma coisa:Olá, Bruno aqui, blz? Quero saber uma coisa:Olá, Bruno aqui, blz? Quero saber uma coisa:Olá, Bruno aqui, blz? Quero saber uma coisa:Respostssa 1",
-                    points: 5},
-                    {id: 2,
-                    text: "Resposta 2",
-                    points: 3},
-                ],
-                color: '#0D5763',
-                categoryName: 'Precificação'
-            },
-            {
-                id: 2,
-                person: {
-                    name: "Dani Magero",
-                    img: "https://www.investeducar.com.br/wp-content/uploads/2013/07/curso-de-departamento-pessoal-rotinas-diarias-300x300.jpg"
-                },
-                phrases: [
-                    "Oi, Dani Magero, tudo bem?",
-                    "QUais são as estratégias que você usa para vender os seus projetos ou serviços?",
-                ],
-                answers: [
-                    {id: 1,
-                    text: "Resposta 11",
-                    points: 4},
-                    {id: 2,
-                    text: "Resposta 12",
-                    points: 3},
-                ],
-                color: '#4B9A97',
-                categoryName: 'Vendas'
-            }
-        ];
+        questions = [
+	{
+		id: 1,
+		person: {
+			name: "Bruno Capanema",
+			img: "https://www.investeducar.com.br/wp-content/uploads/2013/07/curso-de-departamento-pessoal-rotinas-diarias-300x300.jpg"
+		},
+		phrases: [
+			"Oi, Bruno Capanema, tudo bem?",
+			"Quais são as estratégias que você usa para vender os seus projetos ou serviços?",
+			"Selecione abaixo a resposta que mais se adequa ao seu cenário ;)"
+
+		],
+		answers: [
+			{
+				id: 1,
+				text: "Faço cada trabalho que aparece sem muito planejamento. Não faço ideia da minha capacidade como gestor.",
+				points: 1
+			},
+			{
+				id: 2,
+				text: "Já comecei a perceber a necessidade de ter um nicho de atuação, mas não sei por onde começar.",
+				points: 2
+			},
+			{
+				id: 3,
+				text: "Já conheço o meu nicho, mas ainda não sei se escolhi corretamente. Já leio e pesquiso sobre empreendedorismo mas ainda não consigo aplicar à minha vida.",
+				points: 3
+			},
+			{
+				id: 4,
+				text: "Já identifiquei o meu nicho e trabalho nele. Estou desenvolvendo as minhas aptidões como gestor do meu negócio.",
+				points: 4
+			},
+			{
+				id: 5,
+				text: "Já domino o meu negócio e conheço o meu potencial empreendedor, bem como meus parceiros preferenciais. Estou na estrada certa para atingir os meus objetivos.",
+				points: 5
+			},
+		],
+		color: '#83B947',
+		categoryName: 'Empreendedorismo'
+	},
+	{
+		id: 2,
+		person: {
+			name: "Dani Magero",
+			img: "https://www.investeducar.com.br/wp-content/uploads/2013/07/curso-de-departamento-pessoal-rotinas-diarias-300x300.jpg"
+		},
+		phrases: [
+			"Olá, eu sou a Dani Magero",
+			"Qual o seu nível de conhecimento do seu negócio e da sua capacidade como gestor?"
+		],
+		answers: [
+			{
+				id: 1,
+				text: "Nunca fiz planejamento, meu escritório cresce de acordo com os trabalhos que entram.",
+				points: 1
+			},
+			{
+				id: 2,
+				text: "Eu já tenho missão, visão e valores do meu negócio e guio minhas decisões baseada nelas.",
+				points: 2
+			},
+			{
+				id: 3,
+				text: "Já tenho meu nicho de atuação, mas não sei como atender exclusivamente a esses clientes.",
+				points: 3
+			},
+			{
+				id: 4,
+				text: "Já tenho planejamento, nicho, metas de médio e longo prazo e já desenvolvi uma estrutura de equipe e gestão.",
+				points: 4
+			},
+			{
+				id: 5,
+				text: "Já trabalho exclusivamente no meu nicho, tenho os meus processos todos mapeados e sei onde quero estar daqui a 2, 5 e 10 anos.",
+				points: 5
+			},
+		],
+		color: '#0D5763',
+		categoryName: 'A base'
+	},
+	{
+		id: 3,
+		person: {
+			name: "Bruno Capanema",
+			img: "https://www.investeducar.com.br/wp-content/uploads/2013/07/curso-de-departamento-pessoal-rotinas-diarias-300x300.jpg"
+		},
+		phrases: [
+			"Oi, Bruno Capanema, tudo bem?",
+			"Como você calcula o valor do seu projeto ou serviço?",
+		],
+		answers: [
+			{
+				id: 1,
+				text: "Depende de quanto preciso no mês. Se preciso de R$ 2.000,00, fecho um projeto por esse valor",
+				points: 1
+			},
+			{
+				id: 2,
+				text: "Eu calculo por m²",
+				points: 2
+			},
+			{
+				id: 3,
+				text: "Pergunto aos meus colegas quanto eles cobrariam por um projeto daquele porte.",
+				points: 3
+			},
+			{
+				id: 4,
+				text: "Já conheço os custos do escritório e tento me basear neles pra cobrar. Mas ainda fico um pouco perdido.",
+				points: 4
+			},
+			{
+				id: 5,
+				text: "Já tenho o meu próprio salário, capital de giro, lucro definido e cobro por uma composição de custos x tempo pra produzir o trabalho",
+				points: 5
+			},
+		],
+		color: '#2A7A77',
+		categoryName: 'Precificação'
+	},
+	{
+		id: 4,
+		person: {
+			name: "Dani Magero",
+			img: "https://www.investeducar.com.br/wp-content/uploads/2013/07/curso-de-departamento-pessoal-rotinas-diarias-300x300.jpg"
+		},
+		phrases: [
+			"Olá, eu sou a Dani Magero",
+			"Quais são as estratégias que você usa pra vender os seus projetos ou serviços?"
+		],
+		answers: [
+			{
+				id: 1,
+				text: "Envio as minhas propostas por e-mail e fico torcendo pra serem aprovadas.",
+				points: 1
+			},
+			{
+				id: 2,
+				text: "Fico inseguro na hora de apresentar o preço e dou todos os descontos necessários pra fechar o contrato.",
+				points: 2
+			},
+			{
+				id: 3,
+				text: "Minha proposta já é o contrato e copiei de um amigo. Nem sempre consigo fazer o meu cliente entender o quanto vale o meu trabalho. Eles sempre acham caro.",
+				points: 3
+			},
+			{
+				id: 4,
+				text: "Já falo dos meus diferenciais como profissional, mas não tenho segurança na hora de negociar o preço. Fico muito nervoso e acabo dando alguns descontos.",
+				points: 4
+			},
+			{
+				id: 5,
+				text: "Mostro ao meu cliente o valor e os diferenciais que o meu trabalho tem, conheço de antemão todos as objeções e gatilhos mentais e respostas necessárias no processo. Meu cliente entende que eu sou a melhor solução pra ele. Dificilmente dou descontos ou perco uma negociação.",
+				points: 5
+			},
+		],
+		color: '#4B9A97',
+		categoryName: 'Vendas'
+	},
+		{
+		id: 5,
+		person: {
+			name: "Bruno Capanema",
+			img: "https://www.investeducar.com.br/wp-content/uploads/2013/07/curso-de-departamento-pessoal-rotinas-diarias-300x300.jpg"
+		},
+		phrases: [
+			"Oi, Bruno Capanema, tudo bem?",
+			"Quais as estratégias de marketing você utiliza?",
+		],
+		answers: [
+			{
+				id: 1,
+				text: "Não utilizo nenhum marketing, não sei nem por onde começar a minha divulgação.",
+				points: 1
+			},
+			{
+				id: 2,
+				text: "Uso as redes sociais, mas elas estão cheias de colegas que também são arquitetos e designers.",
+				points: 2
+			},
+			{
+				id: 3,
+				text: "Tenho site e uso as redes sociais, mas não tenho constância nem certeza sobre o conteúdo que devo divulgar.",
+				points: 3
+			},
+			{
+				id: 4,
+				text: "Já faço posts no blog e nas redes sociais, com conteúdo direcionado ao meu público, mas ainda não tive os resultados que eu esperava.",
+				points: 4
+			},
+			{
+				id: 5,
+				text: "Utilizo marketing digital, de onde capto a maior parte dos meus cliente, faço posts pagos e trabalho com marketing off-line, com assessoria de imprensa e mostras de arquitetura.",
+				points: 5
+			},
+		],
+		color: '#5E9221',
+		categoryName: 'Marketing'
+	}
+
+];
     }
     
 
